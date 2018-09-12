45f6983f3d97f8a49eb7cdf84d18547e80396dde
Przemysław Nowak <przemyslaw.nowak@fingoweb.com>
2018-09-12 23:04:47 +0200

    fix: remove unused vue-material css stylesheet

--- src/components/containers/App/index.ts
+++ src/components/containers/App/index.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import { MdButton, MdEmptyState, MdIcon } from 'vue-material/dist/components'
-import 'vue-material/dist/vue-material.min.css'
 
 Vue.use(MdButton);
 Vue.use(MdEmptyState);
